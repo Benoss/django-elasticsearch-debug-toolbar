d4906eef790979359023e015f6c87c055b7e180e
Benoit Chabord <benauf@gmail.com>
2017-05-12 13:32:06 +1200

    Update test_toolbar.py

--- elastic_panel/test/test_toolbar.py
+++ elastic_panel/test/test_toolbar.py
@@ -6,23 +6,6 @@ settings.configure()
 
 from elastic_panel import panel
 
-
-class ElasticQueryInfo:
-    def __init__(self, method, full_url, path, body, status_code, response, duration):
-        if not body:
-            self.body = ''  # Python 3 TypeError if None
-        else:
-            self.body = _pretty_json(body)
-            if isinstance(self.body, bytes):
-                self.body = self.body.decode('ascii','ignore')
-        self.method = method
-        self.full_url = full_url
-        self.path = path
-        self.status_code = status_code
-        self.response = _pretty_json(response)
-        self.duration = round(duration * 1000, 2)
-        self.hash = hashlib.md5("{}{}".format(self.full_url.encode('ascii', 'ignore'), self.body.encode('ascii', 'ignore'))).hexdigest()
-
 class ImportTest(unittest.TestCase):
     def test_input(self):
         panel.ElasticQueryInfo("GET", "asdasd", "asdasd", "{}", 200, "adssad", 1)
