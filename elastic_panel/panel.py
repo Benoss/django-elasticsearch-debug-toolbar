deec60a61cb2f55c72e68009e3816f5d8367bfc1
Benoit Chabord <benauf@gmail.com>
2017-05-12 13:33:06 +1200

    Update panel.py

--- elastic_panel/panel.py
+++ elastic_panel/panel.py
@@ -31,20 +31,18 @@ def _pretty_json(data):
 class ElasticQueryInfo:
     def __init__(self, method, full_url, path, body, status_code, response, duration):
         if not body:
-            body = b''  # Python 3 TypeError if None
+            self.body = ''  # Python 3 TypeError if None
+        else:
+            self.body = _pretty_json(body)
+            if isinstance(self.body, bytes):
+                self.body = self.body.decode('ascii','ignore')
         self.method = method
         self.full_url = full_url
         self.path = path
-        self.body = _pretty_json(body)
         self.status_code = status_code
         self.response = _pretty_json(response)
         self.duration = round(duration * 1000, 2)
-        encoded_body = self.body
-        if not isinstance(self.body, bytes):
-            encoded_body = encoded_body.encode('ascii', 'ignore')
-        
-        self.hash = hashlib.md5("{}{}".format(self.full_url.encode('ascii', 'ignore'), encoded_body)).hexdigest()
-
+        self.hash = hashlib.md5("{}{}".format(self.full_url.encode('ascii', 'ignore'), self.body.encode('ascii', 'ignore'))).hexdigest()
 
 class ElasticDebugPanel(Panel):
     """
