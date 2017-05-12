634bea8ad5c4d5232fa77442e16042b8d0c776bb
Benoit Chabord <benauf@gmail.com>
2017-05-12 13:38:48 +1200

    Fix the python 3 bytes issues

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(
   name='django-elasticsearch-debug-toolbar',
   packages=find_packages(),
-  version='1.1.0',
+  version='1.2.0',
   description='A Django Debug Toolbar panel for Elasticsearch',
     long_description=open('README.md').read(),
     author='Benoit Chabord',
