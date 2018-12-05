8c466f0264a875aedb4d59da4ad0875725886e9d
Alex Gómez <agomez@bebabum.com>
2018-12-05 13:31:25 +0100

    Added Django Debug Toolbar 1.10+ support

---
+++ elastic_panel/static/elastic_panel/js/elastic_panel.js
@@ -0,0 +1,39 @@
+var toggle = function(elem) {
+if (window.getComputedStyle(elem).display === 'block') {
+    elem.style.display = 'none';
+    return;
+}
+
+elem.style.display = 'block';
+};
+
+var uarr = String.fromCharCode(0x25b6),
+    darr = String.fromCharCode(0x25bc);
+
+var showHandler = function(e) {
+
+    toggle(this.nextElementSibling)
+
+    arrow = document.querySelector('a.elasticShowTemplate .toggleArrow')
+    arrow.textContent = arrow.textContent == uarr ? darr : uarr
+
+    toggle(this.parentNode.nextElementSibling)
+
+    return false
+};
+
+for (var e of document.querySelectorAll('a.elasticShowTemplate')) {
+    e.addEventListener('click', showHandler)
+}
+
+var textHandler = function(e) {
+    selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(this.parentNode.nextElementSibling.querySelector('code'));
+    selection.removeAllRanges();
+    selection.addRange(range);
+};
+
+for (var e of document.querySelectorAll('.selectText')) {
+    e.addEventListener('click', textHandler)
+}
