c46ad61f2b8c949f642677c71f66de22cf0ba013
Νικόλαος-Διγενής Καραγιάννης <digenishjkl@gmail.com>
2026-08-13 16:32:17 +0300

    Use <details> element for template contexts
    DDT already dropped unnecessary javascript in 2018

--- elastic_panel/panel.py
+++ elastic_panel/panel.py
@@ -4,7 +4,6 @@ import threading
 
 from debug_toolbar.panels import Panel
 from debug_toolbar.utils import get_stack, render_stacktrace, tidy_stacktrace
-from django.templatetags.static import static
 from django.utils.translation import gettext_lazy as _
 from elasticsearch.connection.base import Connection
 
@@ -93,12 +92,6 @@ class ElasticDebugPanel(Panel):
     @property
     def title(self):
         return self.nav_title
-
-    @property
-    def scripts(self):
-        scripts = super().scripts
-        scripts.append(static("elastic_panel/js/elastic_panel.js"))
-        return scripts
 
     def process_request(self, request):
         collector.clear_collection()
