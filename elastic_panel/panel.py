Benoss/django-elasticsearch-debug-toolbar

commit b47efee8802e1b058daf3abf5f10f021dca07401

treat '_thread._local' object has no attribute 'collection' error

elastic_panel/panel.py
@@ -15,7 +15,8 @@ class ThreadCollector:
         self.data.collection = []
 
     def collect(self, item):
-        self.data.collection.append(item)
+        if hasattr(self.data, 'collection'):
+            self.data.collection.append(item)
 
     def get_collection(self):
         return getattr(self.data, "collection", [])

commit 360b81777f4b40ea1ce96110c55232f681477aa7

style: apply black formatting to panel.py
The black pre-commit hook has been failing on master since b47efee
introduced a single-quoted string, turning every CI run red at the
'Run pre-commit' step.

elastic_panel/panel.py
@@ -15,7 +15,7 @@ class ThreadCollector:
         self.data.collection = []
 
     def collect(self, item):
-        if hasattr(self.data, 'collection'):
+        if hasattr(self.data, "collection"):
             self.data.collection.append(item)
 
     def get_collection(self):